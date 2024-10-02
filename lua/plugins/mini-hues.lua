b77770cc115d110c9bea1737698e27973f6d523e
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-10-02 16:42:42 -0300

    updts

--- lua/plugins/mini-hues.lua
+++ lua/plugins/mini-hues.lua
@@ -2,5 +2,9 @@ return {
     'echasnovski/mini.hues',
     lazy = false,
     priority = 1000,
-    config = function() require('mini.hues').setup({ background = '#2b1a33', foreground = '#c9c5cb' }) end,
+    config = function()
+        -- require('mini.hues').setup({ background = '#2b1a33', foreground = '#c9c5cb' }) -- purple
+        -- require('mini.hues').setup({ background = '#19213a', foreground = '#c4c6cd' }) -- blue
+        require('mini.hues').setup({ background = '#111111', foreground = '#ba7685' }) -- azure
+    end,
 }
