95e11f7470b5a286e87f16859bc33c02fc46334c
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-12-14 17:20:01 -0300

    refactor(plugins): move mini.icons into mini.base16 dependencies
    BREAKING CHANGE: mini.icons is no longer a standalone plugin configuration

--- lua/plugins/mini-base16.lua
+++ lua/plugins/mini-base16.lua
@@ -2,6 +2,21 @@ return {
   'echasnovski/mini.base16',
   lazy = false,
   priority = 1000,
+  dependencies = {
+    'echasnovski/mini.icons',
+    init = function()
+      local M = require('config.functions')
+
+      -- This is lazy loaded since we not need it immediately available
+      M.au('User', {
+        pattern = 'VeryLazy',
+        group = M.group('crnvl96-mini-icons-lazy-load', { clear = true }),
+        -- For plugins that do not support `mini.icons` yet, we mock `nvim-web-devicons` here
+        callback = function() require('mini.icons').mock_nvim_web_devicons() end,
+      })
+    end,
+    opts = {},
+  },
   opts = {
     dark = {
       base00 = '#181818',
