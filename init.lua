ed3c98d8ffb7ff4e7f52992592152002f2c8d755
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-12-29 13:30:37 -0300

    refactor: add cli tools requirements into init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+for _, cli in ipairs({
+  'zathura', -- pdf viewer
+  'tex-fmt', -- LaTex formatter
+  'tree-sitter', -- Treesitter cli
+  'rg', -- MiniPick
+  'rustc', -- blink.cmp (needs rustc nightly)
+  'npm', -- markdown-preview
+}) do
+  if vim.fn.executable(cli) ~= 1 then
+    vim.notify(cli .. ' is not installed in the system.', vim.log.levels.ERROR)
+    return
+  end
+end
+
 local mini_path = vim.fn.stdpath('data') .. '/site/pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
