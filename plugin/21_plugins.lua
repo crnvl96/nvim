957d3fe6a98a9e77106fda11c2e9a5ff85e0d0a4
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-12-30 01:26:40 -0300

    refactor: better settings for lua_ls

--- plugin/21_plugins.lua
+++ plugin/21_plugins.lua
@@ -227,11 +227,44 @@ later(function()
 
   require('lspconfig').lua_ls.setup({
     capabilities = capabilities,
+    on_init = function(client)
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then return end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        diagnostics = {
+          globals = {
+            'vim',
+            'MiniPick',
+            'MiniClue',
+            'MiniDeps',
+            'MiniNotify',
+            'MiniIcons',
+          },
+        },
+        runtime = {
+          version = 'LuaJIT',
+        },
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME,
+            '${3rd}/luv/library',
+          },
+        },
+      })
+    end,
     settings = {
       Lua = {
-        workspace = { checkThirdParty = false },
-        codeLens = { enable = false },
-        doc = { privateName = { '^_' } },
+        -- Using stylua for formatting.
+        format = { enable = false },
+        hint = {
+          enable = true,
+          arrayIndex = 'Disable',
+        },
+        completion = { callSnippet = 'Replace' },
       },
     },
   })
