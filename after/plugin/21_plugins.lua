15041399d214f7a2b2e33ac2217cc24cb7e13f3f
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2025-01-13 17:03:13 -0300

    feat(oil): adds winbar

--- after/plugin/21_plugins.lua
+++ after/plugin/21_plugins.lua
@@ -134,8 +134,21 @@ end)
 Later(function()
   Add('stevearc/oil.nvim')
 
+  function _G.get_oil_winbar()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+    local dir = require('oil').get_current_dir(bufnr)
+    if dir then
+      return vim.fn.fnamemodify(dir, ':~')
+    else
+      return vim.api.nvim_buf_get_name(0)
+    end
+  end
+
   require('oil').setup({
     watch_for_changes = true,
+    win_options = {
+      winbar = '%!v:lua.get_oil_winbar()',
+    },
     keymaps = {
       ['g?'] = { 'actions.show_help', mode = 'n' },
       ['<CR>'] = 'actions.select',
