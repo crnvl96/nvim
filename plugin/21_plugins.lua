00d52f95099d4a988b0fe01e1f503f97fb088095
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2025-01-16 11:00:21 -0300

    feat(mini.ai): add mini.ai plugin

--- plugin/21_plugins.lua
+++ plugin/21_plugins.lua
@@ -247,6 +247,34 @@ later(function()
 end)
 
 later(function()
+  add('nvim-treesitter/nvim-treesitter-textobjects')
+  local miniai = require('mini.ai')
+
+  require('mini.ai').setup({
+    n_lines = 300,
+    custom_textobjects = {
+      f = miniai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+      g = function()
+        local from = { line = 1, col = 1 }
+        local to = {
+          line = vim.fn.line('$'),
+          col = math.max(vim.fn.getline('$'):len(), 1),
+        }
+        return { from = from, to = to }
+      end,
+    },
+    silent = true,
+    search_method = 'cover',
+    mappings = {
+      around_next = '',
+      inside_next = '',
+      around_last = '',
+      inside_last = '',
+    },
+  })
+end)
+
+later(function()
   require('mini.files').setup({
     mappings = {
       show_help = '?',
