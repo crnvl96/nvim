0c0fa66dcf4529db0f7f3dbb5aaec22355854aa5
Adran Carnavale <adran@hey.com>
2025-06-05 10:22:43 -0300

    feat: merge ai plugins into a single file

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,31 @@
 MiniDeps.later(function()
+  local function build_mcp(params)
+    vim.notify('Building mcphub.nvim', vim.log.levels.INFO)
+
+    local obj = vim
+      .system({ 'npm', 'install', '-g', 'mcp-hub@latest' }, { cwd = params.path })
+      :wait()
+
+    if obj.code == 0 then
+      vim.notify('Building mcphub.nvim done', vim.log.levels.INFO)
+    else
+      vim.notify('Building mcphub.nvim failed', vim.log.levels.ERROR)
+    end
+  end
+
+  MiniDeps.add {
+    source = 'ravitemer/mcphub.nvim',
+    hooks = {
+      post_install = build_mcp,
+      post_checkout = build_mcp,
+    },
+  }
+
   MiniDeps.add 'olimorris/codecompanion.nvim'
+
+  require('mcphub').setup {
+    config = vim.fn.expand '~/.config/nvim/mcphub/servers.json',
+  }
 
   local function retrieve_llm_key(key_name)
     local filepath = vim.fn.stdpath 'config' .. '/.env'
