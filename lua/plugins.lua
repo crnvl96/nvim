c08bb323b515701a46da8a87fea4712f402774cd
Adran Carnavale <adran@hey.com>
2025-06-13 11:32:35 -0300

    feat: add conditional tool formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -332,6 +332,11 @@ MiniDeps.later(function()
   vim.o.formatexpr = 'v:lua.require\'conform\'.formatexpr()'
   vim.g.conform = true
 
+  local function get_root_dir(root_files, bufnr)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    return vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1])
+  end
+
   require('conform').setup {
     notify_on_error = true,
     formatters = {
@@ -341,14 +346,32 @@ MiniDeps.later(function()
     },
     formatters_by_ft = {
       ['_'] = { 'trim_whitespace', 'trim_newlines' },
-      lua = { 'stylua' },
+      css = { 'prettier' },
+      javascript = function(bufnr)
+        local biome_root_files = { 'biome.json', 'biome.jsonc' }
+
+        if get_root_dir(biome_root_files, bufnr) then
+          return { 'biome', 'biome-check', 'biome-organize-imports' }
+        else
+          return { 'prettier' }
+        end
+      end,
+      javascriptreact = function(bufnr)
+        local biome_root_files = { 'biome.json', 'biome.jsonc' }
+
+        if get_root_dir(biome_root_files, bufnr) then
+          return { 'biome', 'biome-check', 'biome-organize-imports' }
+        else
+          return { 'prettier' }
+        end
+      end,
       json = { 'prettier' },
+      lua = { 'stylua' },
       markdown = { 'prettier', 'injected' },
       python = { 'ruff_fix', 'ruff_organize_imports', 'ruff_format' },
+      ruby = { 'rubocop' },
       typescript = { 'prettier' },
       typescriptreact = { 'prettier' },
-      css = { 'prettier' },
-      javascript = { 'prettier' },
     },
     format_on_save = function()
       return vim.g.conform and { timeout_ms = 3000, lsp_format = 'fallback' }
