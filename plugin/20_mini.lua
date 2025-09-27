37e4310f404250fd25ea66c34b9928711b7b8490
Adran Carnavale <adran@hey.com>
2025-09-26 23:18:37 -0300

    come back

--- plugin/20_mini.lua
+++ plugin/20_mini.lua
@@ -49,21 +49,21 @@ vim.ui.select = function(items, opts, on_choice)
   return MiniPick.ui_select(items, opts, on_choice, start_opts)
 end
 
--- require('mini.files').setup({
---   mappings = {
---     show_help = '?',
---     go_in = '',
---     go_out = '',
---     go_in_plus = '<CR>',
---     go_out_plus = '-',
---   },
--- })
---
--- --- Open MiniFiles at the current directory
--- local function open_file_explorer()
---   local bufname = vim.api.nvim_buf_get_name(0)
---   local path = vim.fn.fnamemodify(bufname, ':p')
---   if path and vim.uv.fs_stat(path) then MiniFiles.open(bufname, false) end
--- end
---
--- vim.keymap.set('n', '-', function() return open_file_explorer() end)
+require('mini.files').setup({
+  mappings = {
+    show_help = '?',
+    go_in = '',
+    go_out = '',
+    go_in_plus = '<CR>',
+    go_out_plus = '-',
+  },
+})
+
+--- Open MiniFiles at the current directory
+local function open_file_explorer()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  local path = vim.fn.fnamemodify(bufname, ':p')
+  if path and vim.uv.fs_stat(path) then MiniFiles.open(bufname, false) end
+end
+
+vim.keymap.set('n', '-', function() return open_file_explorer() end)
