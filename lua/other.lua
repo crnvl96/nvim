bcad215fb5cc26f3bcb3a5debe40047acb02d4a4
Adran Carnavale <adran@hey.com>
2025-10-01 14:56:26 -0300

    add vim tmux navigator

--- lua/other.lua
+++ lua/other.lua
@@ -108,3 +108,9 @@ require('conform').setup({
 
 vim.api.nvim_create_user_command('Fmt', format_current, { nargs = 0 })
 vim.api.nvim_create_user_command('ToggleFmt', toggle_autoformat, { nargs = 0 })
+
+vim.keymap.set('n', '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>')
+vim.keymap.set('n', '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>')
+vim.keymap.set('n', '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>')
+vim.keymap.set('n', '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>')
+vim.keymap.set('n', '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>')
