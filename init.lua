0789186d9ea2ee975aa2e334d22386c9a985a153
Adran Carnavale <adran@hey.com>
2025-10-13 20:05:52 -0300

    small changes

--- init.lua
+++ init.lua
@@ -56,6 +56,14 @@ vim.opt.pumborder = 'double'
 
 vim.cmd 'filetype plugin indent on'
 
+_G.Config = {}
+
+local gr = vim.api.nvim_create_augroup('custom-config', {})
+_G.Config.new_autocmd = function(event, pattern, callback, desc)
+    local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+    vim.api.nvim_create_autocmd(event, opts)
+end
+
 require 'keymaps'
 require 'pack'
 require 'treesitter'
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,49 +1,52 @@
-local set = vim.keymap.set
+local s = vim.keymap.set
+local ex = function(mode, lhs, rhs) s(mode, lhs, rhs, { expr = true }) end
 
-set('n', '<C-Down>', '<Cmd>resize -5<CR>')
-set('n', '<C-Up>', '<Cmd>resize +5<CR>')
-set('n', '<C-Left>', '<Cmd>vertical resize -20<CR>')
-set('n', '<C-Right>', '<Cmd>vertical resize +20<CR>')
-set('n', '<C-d>', '<C-d>zz')
-set('n', '<C-u>', '<C-u>zz')
-set('n', '<C-h>', '<C-w>h')
-set('n', '<C-j>', '<C-w>j')
-set('n', '<C-k>', '<C-w>k')
-set('n', '<C-l>', '<C-w>l')
-set('x', '<', '<gv')
-set('x', '>', '>gv')
-set('x', 'p', 'P')
-set({ 'n', 'x', 'o' }, 'Y', 'yg_')
-set({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
-set({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+s('n', '<C-Down>', '<Cmd>resize -5<CR>')
+s('n', '<C-Up>', '<Cmd>resize +5<CR>')
+s('n', '<C-Left>', '<Cmd>vertical resize -20<CR>')
+s('n', '<C-Right>', '<Cmd>vertical resize +20<CR>')
+s('n', '<C-d>', '<C-d>zz')
+s('n', '<C-u>', '<C-u>zz')
+s('n', '<C-h>', '<C-w>h')
+s('n', '<C-j>', '<C-w>j')
+s('n', '<C-k>', '<C-w>k')
+s('n', '<C-l>', '<C-w>l')
+s('x', '<', '<gv')
+s('x', '>', '>gv')
+s('x', 'p', 'P')
+s({ 'n', 'x', 'o' }, 'Y', 'yg_')
+ex({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'")
+ex({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'")
 
-set('n', 'H', 'mzgggqG`z<Cmd>delmarks z<CR>zz')
-set('x', 'H', 'gqzz')
+s('n', 'H', 'mzgggqG`z<Cmd>delmarks z<CR>zz')
+s('x', 'H', 'gqzz')
 
-set({ 'i', 'n', 's' }, '<esc>', function()
+s({ 'i', 'n', 's' }, '<esc>', function()
     vim.cmd.nohlsearch()
     return '<esc>'
-end, { expr = true })
+end)
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-set('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true })
-set('x', 'n', "'Nn'[v:searchforward]", { expr = true })
-set('o', 'n', "'Nn'[v:searchforward]", { expr = true })
-set('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true })
-set('x', 'N', "'nN'[v:searchforward]", { expr = true })
-set('o', 'N', "'nN'[v:searchforward]", { expr = true })
+s('n', 'n', "'Nn'[v:searchforward].'zv'")
+s('x', 'n', "'Nn'[v:searchforward]")
+s('o', 'n', "'Nn'[v:searchforward]")
+s('n', 'N', "'nN'[v:searchforward].'zv'")
+s('x', 'N', "'nN'[v:searchforward]")
+s('o', 'N', "'nN'[v:searchforward]")
 
-set('i', ',', ',<c-g>u')
-set('i', '.', '.<c-g>u')
-set('i', ';', ';<c-g>u')
-set({ 'i', 'x', 'n' }, '<C-s>', '<Cmd>noh<CR><Esc><Cmd>write!<CR><Esc>')
+s('i', ',', ',<c-g>u')
+s('i', '.', '.<c-g>u')
+s('i', ';', ';<c-g>u')
+s({ 'i', 'x', 'n' }, '<C-s>', '<Cmd>noh<CR><Esc><Cmd>write!<CR><Esc>')
 
-set('c', '<m-left>', function() return vim.fn.wildmenumode() and '<C-e><c-left>' or '<c-left>' end, { expr = true })
-set('c', '<m-down>', function() return vim.fn.wildmenumode() and '<C-e><c-down>' or '<c-down>' end, { expr = true })
-set('c', '<m-up>', function() return vim.fn.wildmenumode() and '<C-e><c-up>' or '<c-up>' end, { expr = true })
-set('c', '<m-right>', function() return vim.fn.wildmenumode() and '<C-e><c-right>' or '<c-right>' end, { expr = true })
+local menu = vim.fn.wildmenumode
 
-set('n', '<Leader>x', function()
+ex('c', '<m-left>', function() return menu() and '<C-e><c-left>' or '<c-left>' end)
+ex('c', '<m-down>', function() return menu() and '<C-e><c-down>' or '<c-down>' end)
+ex('c', '<m-up>', function() return menu() and '<C-e><c-up>' or '<c-up>' end)
+ex('c', '<m-right>', function() return menu() and '<C-e><c-right>' or '<c-right>' end)
+
+s('n', '<Leader>x', function()
     if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then
         vim.cmd 'cclose'
     else
