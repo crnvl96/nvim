f33bc93aa5a677d91ff927b9fdac4a7a25fc6a79
Adran Carnavale <adran@hey.com>
2025-10-15 16:44:47 -0300

    update steps ahead

--- lua/user/mini.lua
+++ lua/user/mini.lua
@@ -98,7 +98,7 @@ require('mini.notify').setup {
 require('mini.jump2d').setup {
     spotter = require('mini.jump2d').gen_spotter.pattern '[^%s%p]+',
     labels = "asdfghjkl'",
-    view = { dim = true, n_steps_ahead = 1 },
+    view = { dim = true, n_steps_ahead = 2 },
     mappings = { start_jumping = 'S' },
 }
 
