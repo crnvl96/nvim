1baec0f06464b19afb877b43c989c936c58dabf1
Adran Carnavale <adran@hey.com>
2025-10-24 17:12:23 -0300

    updts

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 vim.cmd 'setlocal spell wrap'
-
+vim.cmd 'setlocal colorcolumn=81'
 vim.cmd 'setlocal foldmethod=expr foldexpr=v:lua.vim.treesitter.foldexpr()'
 
 vim.bo.shiftwidth = 2
--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,3 +1,4 @@
 vim.cmd 'setlocal spell wrap'
+vim.cmd 'setlocal colorcolumn=81'
 
 vim.keymap.del('n', 'gO', { buffer = 0 })
