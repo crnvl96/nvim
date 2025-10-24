203e1c4a766ca6271c5dae3e9546e055ea5cf73e
Adran Carnavale <adran@hey.com>
2025-10-24 09:03:34 -0300

    updates

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,6 +2,8 @@ vim.cmd 'setlocal spell wrap'
 
 vim.cmd 'setlocal foldmethod=expr foldexpr=v:lua.vim.treesitter.foldexpr()'
 
+vim.bo.shiftwidth = 2
+
 vim.keymap.del('n', 'gO', { buffer = 0 })
 
 vim.b.minisurround_config = {
--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -102,33 +102,33 @@ end)
 
 later(function()
   add 'Olical/conjure'
-  add 'm00qek/baleia.nvim'
-
-  vim.g.conjure_baleia = require('baleia').setup { line_starts_at = 3 }
-
-  vim.api.nvim_create_user_command(
-    'BaleiaColorize',
-    function() vim.g.conjure_baleia.once(vim.api.nvim_get_current_buf()) end,
-    { bang = true }
-  )
-
-  vim.api.nvim_create_user_command('BaleiaLogs', vim.g.conjure_baleia.logger.show, { bang = true })
-
-  -- Print color codes if baleia.nvim is available
-  vim.g['conjure#log#strip_ansi_escape_sequences_line_limit'] = 1
-
-  -- Disable diagnostics in log buffer and colorize it
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    pattern = 'conjure-log-*',
-    callback = function()
-      local buffer = vim.api.nvim_get_current_buf()
-      vim.diagnostic.enable(false, { bufnr = buffer })
-      if vim.g.conjure_baleia then vim.g.conjure_baleia.automatically(buffer) end
-    end,
-  })
-
-  MiniMisc.put(vim.g.conjure_baleia)
-
-  require('conjure.main').main()
-  require('conjure.mapping')['on-filetype']()
+  -- add 'm00qek/baleia.nvim'
+  --
+  -- vim.g.conjure_baleia = require('baleia').setup { line_starts_at = 3 }
+  --
+  -- vim.api.nvim_create_user_command(
+  --   'BaleiaColorize',
+  --   function() vim.g.conjure_baleia.once(vim.api.nvim_get_current_buf()) end,
+  --   { bang = true }
+  -- )
+  --
+  -- vim.api.nvim_create_user_command('BaleiaLogs', vim.g.conjure_baleia.logger.show, { bang = true })
+  --
+  -- -- Print color codes if baleia.nvim is available
+  -- vim.g['conjure#log#strip_ansi_escape_sequences_line_limit'] = 1
+  --
+  -- -- Disable diagnostics in log buffer and colorize it
+  -- vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  --   pattern = 'conjure-log-*',
+  --   callback = function()
+  --     local buffer = vim.api.nvim_get_current_buf()
+  --     vim.diagnostic.enable(false, { bufnr = buffer })
+  --     if vim.g.conjure_baleia then vim.g.conjure_baleia.automatically(buffer) end
+  --   end,
+  -- })
+  --
+  -- MiniMisc.put(vim.g.conjure_baleia)
+  --
+  -- require('conjure.main').main()
+  -- require('conjure.mapping')['on-filetype']()
 end)
