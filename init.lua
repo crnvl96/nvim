66559835bfd14ee8177badaaefbb1b90696790bc
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-17 14:54:19 -0300

    Setup path

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ require('mini.deps').setup()
 
 local node_bin = vim.env.HOME .. '/.local/share/mise/installs/node/24.11.0/bin'
 vim.g.node_host_prog = node_bin .. '/node'
--- vim.env.PATH = node_bin .. vim.env.PATH
+vim.env.PATH = node_bin .. ':' .. vim.env.PATH
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('crnvl96-highlight-after-yank', {}),
