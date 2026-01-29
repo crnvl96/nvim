a32a6ac1cfdfe36a92e3744aa761a410b065b527
Adran Carnavale <adran.carnavale@gmail.com>
2026-01-29 11:58:04 -0300

    Updates

--- plugin/15_cmdline.lua
+++ plugin/15_cmdline.lua
@@ -64,35 +64,10 @@ ltr(function()
               )
             )
         end
-        if ev.event == 'CmdlineChanged' and should_enable_autocomplete() then
-          -- vim.opt.wildmode = 'noselect:lastused,full'
-          vim.fn.wildtrigger()
-        end
-        -- if ev.event == 'CmdlineLeave' then vim.opt.wildmode = 'noselect:lastused,full' end
+
+        if ev.event == 'CmdlineChanged' and should_enable_autocomplete() then vim.fn.wildtrigger() end
       end),
       100
     ),
   })
-
-  vim.keymap.set('n', '<leader>f', ':find<space>', { desc = 'Fuzzy find' })
-
-  vim.keymap.set('c', '<m-e>', '<home><s-right><c-w>edit<end>', { desc = 'Change command to :edit' })
-
-  vim.keymap.set('c', '<m-d>', function()
-    if not is_cmdline_type_find() then
-      vim.notify('This binding should be used with :find', vim.log.levels.ERROR)
-      return
-    end
-    local cmdline_arg = vim.fn.split(vim.fn.getcmdline(), ' ')[2]
-    if vim.uv.fs_realpath(vim.fn.expand(cmdline_arg)) == nil then
-      vim.notify('The second argument should be a valid path', vim.log.levels.ERROR)
-      return
-    end
-    local keys = vim.api.nvim_replace_termcodes('<C-U>edit ' .. vim.fs.dirname(cmdline_arg), true, true, true)
-    vim.fn.feedkeys(keys, 'c')
-  end, { desc = 'Edit the dir for the path' })
-
-  vim.keymap.set('c', '<c-v>', '<home><s-right><c-w>vs<end>', { desc = 'Change command to :vs' })
-  vim.keymap.set('c', '<c-s>', '<home><s-right><c-w>sp<end>', { desc = 'Change command to :sp' })
-  vim.keymap.set('c', '<c-t>', '<home><s-right><c-w>tabe<end>', { desc = 'Change command to :tabe' })
 end)
