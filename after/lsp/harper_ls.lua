a6ebfddcec618578c97b0216fd322ba3d7f8bca0
Adran Carnavale <adran.carnavale@gmail.com>
2026-03-02 09:25:02 -0300

    Updates

---
+++ after/lsp/harper_ls.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+  settings = {
+    ['harper-ls'] = {
+      linters = {
+        SentenceCapitalization = false,
+        SpellCheck = false,
+      },
+    },
+  },
+}
