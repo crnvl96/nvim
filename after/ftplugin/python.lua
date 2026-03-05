032f4b2df275f55e4893bd29b8be2b371ac82120
Adran Carnavale <adran.carnavale@gmail.com>
2026-03-05 17:00:39 -0300

    updates

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+-- Indentation
+vim.g.pyindent_open_paren = 'shiftwidth()'
+vim.g.pyindent_continue = 'shiftwidth()'
+
+-- mini.indentscope
+vim.b.miniindentscope_config = { options = { border = 'top' } }
