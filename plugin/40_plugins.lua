06d2f2c25ba06aa0c523e2046e5b9895687fe82c
Adran Carnavale <adran.carnavale@gmail.com>
2026-03-09 10:36:46 -0300

    Updates

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -1,12 +1,13 @@
 Config.now(function() require('crnvl.plug.tokyonight') end)
 Config.now(function() require('crnvl.plug.mini-icons') end)
 
-Config.now_if_args(function() require('crnvl.plug.schemastore') end)
+Config.now_if_args(function() require('crnvl.plug.schemastores') end)
 Config.now_if_args(function() require('crnvl.plug.sleuth') end)
 Config.now_if_args(function() require('crnvl.plug.lualine') end)
 Config.now_if_args(function() require('crnvl.plug.ts-autotag') end)
 Config.now_if_args(function() require('crnvl.plug.ts-comments') end)
 Config.now_if_args(function() require('crnvl.plug.vim-tmux-navigator') end)
+Config.now_if_args(function() require('crnvl.plug.gitsigns') end)
 
 -- wtiting
 Config.now_if_args(function() require('crnvl.plug.image') end)
@@ -19,6 +20,7 @@ Config.now_if_args(function() require('crnvl.plug.treesitter') end)
 Config.now_if_args(function() require('crnvl.plug.lspconfig') end)
 Config.now_if_args(function() require('crnvl.plug.conform') end)
 
+-- mini
 Config.now_if_args(function() require('crnvl.plug.mini-bufremove') end)
 Config.now_if_args(function() require('crnvl.plug.mini-indentscope') end)
 Config.now_if_args(function() require('crnvl.plug.mini-completion') end)
@@ -32,9 +34,11 @@ Config.now_if_args(function() require('crnvl.plug.mini-files') end)
 Config.now_if_args(function() require('crnvl.plug.mini-pick') end)
 Config.now_if_args(function() require('crnvl.plug.mini-clue') end)
 
+-- mini pt. 2
 Config.now_if_args(function() require('mini.align').setup() end)
 Config.now_if_args(function() require('mini.operators').setup() end)
 Config.now_if_args(function() require('mini.move').setup() end)
 Config.now_if_args(function() require('mini.surround').setup() end)
 Config.now_if_args(function() require('mini.splitjoin').setup() end)
 Config.now_if_args(function() require('mini.cmdline').setup() end)
+Config.now_if_args(function() require('mini.git').setup() end)
