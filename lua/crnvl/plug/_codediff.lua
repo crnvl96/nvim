6162174f83551f3cfff55baa7cac43e6df6b98fc
Adran Carnavale <adran.carnavale@gmail.com>
2026-03-15 10:58:17 -0300

    Updates

--- lua/crnvl/plug/_codediff.lua
+++ lua/crnvl/plug/_codediff.lua
@@ -1,6 +1,13 @@
 vim.pack.add({ 'https://github.com/esmuellert/codediff.nvim' })
 
-require('codediff').setup()
+require('codediff').setup({
+  diff = {
+    layout = 'inline',
+    compute_moves = true,
+    jump_to_first_change = false,
+  },
+  explorer = { width = 30 },
+})
 
 vim.keymap.set(
   'n',
