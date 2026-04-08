3cf0a5daccb66f0192eb667722ede09a8aa14f58
Adran Carnavale <adran.carnavale@gmail.com>
2026-04-07 22:56:59 -0300

    Updates

--- init.lua
+++ init.lua
@@ -347,16 +347,14 @@ require('mini.ai').setup({
       a = '@function.outer',
       i = '@function.inner',
     }),
-    g = {
-      from = {
-        line = 1,
-        col = 1,
-      },
-      to = {
+    g = function()
+      local from = { line = 1, col = 1 }
+      local to = {
         line = vim.fn.line('$'),
         col = math.max(vim.fn.getline('$'):len(), 1),
-      },
-    },
+      }
+      return { from = from, to = to }
+    end,
   },
 })
 
