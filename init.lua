cc9f2e6f22b4b854a56fc9dae7f17ffcf2ef75dc
Adran Carnavale <adran.carnavale@gmail.com>
2026-04-08 16:48:12 -0300

    Updates

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ vim.g.loaded_netrwPlugin = 1
 require('vim._core.ui2').enable({
   enable = true,
   msg = {
-    targets = 'msg',
+    -- targets = 'msg',
     cmd = { height = 0.5 },
     dialog = { height = 0.5 },
     msg = { height = 0.5, timeout = 4000 },
