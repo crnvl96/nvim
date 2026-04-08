bea027ba064db1c509d934e655db689df97e85f6
Adran Carnavale <adran.carnavale@gmail.com>
2026-04-08 16:47:44 -0300

    Updates

--- plugin/60_keymaps.lua
+++ plugin/60_keymaps.lua
@@ -131,24 +131,16 @@ Config.set('n', '<leader>la', '<cmd>FzfLua lsp_code_actions<cr>', { desc = 'LSP 
 
 require('mini.clue').setup({
   triggers = {
-    -- Leader triggers
     { mode = { 'n', 'x' }, keys = '<Leader>' },
-    -- `[` and `]` keys
     { mode = 'n', keys = '[' },
     { mode = 'n', keys = ']' },
-    -- Built-in completion
     { mode = 'i', keys = '<C-x>' },
-    -- `g` key
     { mode = { 'n', 'x' }, keys = 'g' },
-    -- Marks
     { mode = { 'n', 'x' }, keys = "'" },
     { mode = { 'n', 'x' }, keys = '`' },
-    -- Registers
     { mode = { 'n', 'x' }, keys = '"' },
     { mode = { 'i', 'c' }, keys = '<C-r>' },
-    -- Window commands
     { mode = 'n', keys = '<C-w>' },
-    -- `z` key
     { mode = { 'n', 'x' }, keys = 'z' },
   },
   clues = {
